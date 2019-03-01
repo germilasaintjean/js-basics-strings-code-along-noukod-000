06fa19a759754bba081c935066cab0cc2a203085
Saint-Jean Germila <germila@noukod.com>
2019-03-01 20:08:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code in this
 const currentUser = "Grace Hopper";
-const welcomeMessage = 'Welcome to Flatbook';
+const welcomeMessage = "Welcome to Flatbook";
 const welcomeMessage = curretUser;
+
